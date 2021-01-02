9253c76d18d85734003ee32fb692c5333c6a711e
hyperflow <hyperflow@kakao.com>
2021-01-03 05:45:11 +0900

    feat(Button): Button 컴포넌트 추가

--- package/board/src/components/Button.stories.tsx
+++ package/board/src/components/Button.stories.tsx
@@ -1,13 +1,51 @@
 import React from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
-import { Button } from './Button';
+import { Button, buttonProps } from './Button';
 
 export default {
-  title: 'component/Button',
+  title: 'component/Button/Button',
   component: Button,
+  argTypes: {
+    bgColor: { control: 'color' },
+    color: { control: 'color' },
+  },
 } as Meta;
 
-const Template: Story = args => <Button {...args} />;
+const Template: Story<buttonProps> = args => <Button {...args} />;
 
 export const Primary = Template.bind({});
-Primary.args = {};
+Primary.args = {
+  label: '제출',
+
+  width: '80px',
+  height: '36px',
+  radius: '5px',
+  bgColor: '#1EA7FD',
+  color: '#fff',
+
+  fontSize: '14px',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  label: '제출',
+
+  width: '80px',
+  height: '36px',
+  radius: '5px',
+  bgColor: '#fff',
+  color: '#000',
+
+  fontSize: '14px',
+};
+
+export const Destructive = Template.bind({});
+Destructive.args = {
+  label: '삭제',
+
+  radius: '5px',
+  borderColor: '#f00',
+  color: '#f00',
+
+  fontSize: '14px',
+};
--- package/board/src/components/Button.tsx
+++ package/board/src/components/Button.tsx
@@ -1,21 +1,34 @@
 import React from 'react';
 import styled from 'styled-components';
 
-export const Button = () => {
-  return <StyledButton>hi</StyledButton>;
-};
-
 interface styledButtonProps {
   width?: string;
   height?: string;
   bgColor?: string;
+  borderColor?: string;
   color?: string;
+  fontSize?: string;
+  radius?: string;
+  padding?: string;
 }
 
+export interface buttonProps extends styledButtonProps {
+  label: string;
+}
+
+export const Button = (args: buttonProps) => {
+  return <StyledButton {...args}>{args.label}</StyledButton>;
+};
+
 const StyledButton = styled.button<styledButtonProps>`
-  width: ${p => p.width || '200px'};
-  height: ${p => p.height || '80px'};
-  border-radius: 50%;
-  background-color: ${p => p.bgColor || 'orange'};
+  border: 1px solid ${p => p.borderColor || 'rgb(215, 215, 215)'};
+
+  width: ${p => p.width || '80px'};
+  height: ${p => p.height || '36px'};
+  border-radius: ${p => p.radius || '5px'};
+  background-color: ${p => p.bgColor || '#fff'};
   color: ${p => p.color || 'white'};
+  padding: ${p => p.padding || '5px'};
+
+  font-size: ${p => p.fontSize || '14px'};
 `;
