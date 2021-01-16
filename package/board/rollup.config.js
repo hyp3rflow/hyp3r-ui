614cf4457b1f6710da6c85443131b804d7a2480d
hyperflow <hyperflow@kakao.com>
2021-01-16 14:34:26 +0900

    package: css plugin, toast-editor 추가

--- package/board/rollup.config.js
+++ package/board/rollup.config.js
@@ -2,6 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 import url from '@rollup/plugin-url';
 import typescript from '@rollup/plugin-typescript';
+import css from 'rollup-plugin-import-css';
 import pkg from './package.json';
 
 const extensions = ['.js', '.jsx', '.ts', '.tsx'];
@@ -10,7 +11,13 @@ process.env.BABEL_ENV = 'production';
 
 export default {
   input: './src/index.ts',
-  external: ['styled-components', 'react', 'react-dom', 'react-icons'],
+  external: [
+    'styled-components',
+    'react',
+    'react-dom',
+    'react-icons',
+    '@toast-ui/react-editor',
+  ],
   plugins: [
     typescript({ typescript: require('typescript') }),
     resolve({ extensions }),
@@ -21,6 +28,7 @@ export default {
       babelHelpers: 'runtime',
     }),
     url(),
+    css(),
   ],
   output: [{ file: pkg.module, format: 'es' }],
 };
